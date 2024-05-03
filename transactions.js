40776163b9f9a9d9845074bd264dd12beb2a9021
x1tz <hugoconsciencia@gmail.com>
2024-05-03 12:03:30 +0100

    Create transactions.js

---
+++ transactions.js
@@ -0,0 +1,46 @@
+const Web3 = require('web3');
+
+// Replace with your Quorum node provider URL
+const providerUrl = 'http://localhost:8545';
+
+// Create a Web3 instance connected to your node
+const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
+
+let myTransactionsList = []; //Initialize an empty list
+
+async function getAllTransactions() {
+    const blockNumber = await web3.eth.getBlockNumber(); //Get most recent block number
+    //console.log(blockNumber);
+
+    for(let k = 0; k <= blockNumber; k++){
+        const block = await web3.eth.getBlock(k); //Get block object by number
+        //console.log(block.transactions);
+        const hash = block.hash;
+        const numberTx = await web3.eth.getBlockTransactionCount(hash);
+        if(numberTx > 0){
+            for(let i = 0; i < numberTx; i++){
+                const txfound = await web3.eth.getTransactionFromBlock(hash, i);
+                //console.log("txfound ->"  + txfound);
+                myTransactionsList.push(txfound);
+            }
+        }
+    }
+    //console.log(myTransactionsList);
+}
+
+//Prints hashes of the transactions found
+async function printTx(){
+    for(let i=0; i<myTransactionsList.length; i++){
+        console.log(myTransactionsList[i].hash);
+    }
+}
+
+//async makes rest of code to wait for getAllTransactions()
+(async () => {
+    await getAllTransactions();
+    console.log(myTransactionsList); //Print all transactions retrieved
+    printTx();
+})();
+
+
+
